aea73c475d56a6727b947b1426843cb3f423b256
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2015-05-01 16:36:26 +0200

    Added ParcelerArgsBundler and CastedArrayListArgsBundler

---
+++ annotation/src/main/java/com/hannesdorfmann/fragmentargs/bundler/CastedArrayListArgsBundler.java
@@ -0,0 +1,35 @@
+package com.hannesdorfmann.fragmentargs.bundler;
+
+import android.os.Bundle;
+import android.os.Parcelable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This {@link ArgsBundler} takes a java.util.List and casts it to an ArrayList. So it assumes that
+ * the List is instance of ArrayList.
+ * <p>
+ * With this ArgsBundler you can annotate fields of type java.util.List like that
+ * <code>@Arg(bundler
+ * = CastedArrayListArgsBundler.class) List<Foo> foos></code>
+ * </p>
+ *
+ * @author Hannes Dorfmann
+ * @since 2.1
+ */
+public class CastedArrayListArgsBundler implements ArgsBundler<List<? extends Parcelable>> {
+
+  @Override public void put(String key, List<? extends Parcelable> value, Bundle bundle) {
+    if (!(value instanceof ArrayList)) {
+      throw new ClassCastException(
+          "CastedArrayListArgsBundler assumes that the List is instance of ArrayList, but it's instance of "
+              + value.getClass().getCanonicalName());
+    }
+
+    bundle.putParcelableArrayList(key, (ArrayList) value);
+  }
+
+  @Override public List<? extends Parcelable> get(String key, Bundle bundle) {
+    return bundle.getParcelableArrayList(key);
+  }
+}
---
+++ bundler-parceler/src/main/java/com/hannesdorfmann/fragmentargs/bundler/ParcelerArgsBundler.java
@@ -0,0 +1,21 @@
+package com.hannesdorfmann.fragmentargs.bundler;
+
+import android.os.Bundle;
+import org.parceler.Parcels;
+
+/**
+ * A {@link ArgsBundler} implementation for Parceler
+ *
+ * @author Hannes Dorfmann
+ * @since 2.1
+ */
+public class ParcelerArgsBundler implements ArgsBundler<Object> {
+
+  @Override public void put(String key, Object value, Bundle bundle) {
+    bundle.putParcelable(key, Parcels.wrap(value));
+  }
+
+  @Override public Object get(String key, Bundle bundle) {
+    return Parcels.unwrap(bundle.getParcelable(key));
+  }
+}
