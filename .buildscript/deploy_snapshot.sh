4d615d0c13c97259d8a0d75f6e86dfc349b58c93
Andreas Böhm <andreas@boehm.cx>
2018-06-29 16:26:29 +0200

    allow jdk8 to be used for snapshot deployment

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -6,7 +6,7 @@
 # http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
 SLUG="sockeqwe/fragmentargs"
-JDK="oraclejdk7"
+JDK="oraclejdk8"
 BRANCH="master"
 
 set -e
